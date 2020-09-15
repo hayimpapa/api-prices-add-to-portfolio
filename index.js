465fad0527d4ce41aed4f9d7b5d89563497c3f31
Gabor <Gabor@Devenyi.com.au>
2020-09-15 22:10:15 +1000

    Updated table to be nicer and code to be more readable

--- index.js
+++ index.js
@@ -4,12 +4,32 @@ async function getPrice() {
   const response = await fetch(api_url);
   const apiData = await response.json();
   var cryptoData = [];
+  
+  document.getElementById("crypto").innerHTML =`
+    <!-- header row for explanation -->
+  <tr>
+    <td><b>Cryptocurrency name</b></td>
+    <td><b>Symbol</b></td>
+    <td><b>Current price in USD</b></td>
+    <td><b>Holdings</b></td>
+    <td><b>Amount to add in USD</b></td>
+    <td><b>Action</b></td>
+  </tr>
+  `
   for (let i=0;i<apiData.length;i++){
+    var currentName = apiData[i].name;
     var currentSymbol = apiData[i].symbol;
     var currentPrice = apiData[i].current_price;
-//    document.getElementById("crypto").innerHTML += '<tr><td>'+currentSymbol+'</td>'+'<td>$ '+ currentPrice+ '</td><td>0</td><td><input id='+currentSymbol+'_add_amount type=text placeholder=\"Add '+currentSymbol+' amount\"></td><td><input type=button value=Submit id='+currentSymbol+'_add_submit></td></tr>';
-    document.getElementById("crypto").innerHTML += `<tr><td>${currentSymbol}</td><td>$ ${currentPrice} </td><td>0</td><td><input id=${currentSymbol}_add_amount type=text placeholder=\"Add ${currentSymbol} amount\"></td><td><input type=button value=Submit id=${currentSymbol}_add_submit></td></tr>`;
-    cryptoData.push(currentSymbol, currentPrice);
+    document.getElementById("crypto").innerHTML += `
+    <tr>
+      <td>${currentName}</td>
+      <td>${currentSymbol}</td>
+      <td>$ ${currentPrice} </td>
+      <td>0</td>
+      <td><input id=${currentSymbol}_add_amount type=text placeholder=\"Add ${currentSymbol} amount\"></td>
+      <td><input type=button value=Submit id=${currentSymbol}_add_submit></td>
+    </tr>`;
+    cryptoData.push(currentName, currentSymbol, currentPrice);
 
   }
 console.log(cryptoData);
